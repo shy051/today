aa9c22eabd9f71a450400f8fb0ab73e2b2d55600
Kenia Duque <kduque@ucsd.edu>
2015-02-26 20:53:16 -0800

    a sign up button

--- routes/story_1.js
+++ routes/story_1.js
@@ -1,6 +1,10 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
-	console.log(data);
-	res.render('story_1',data);
+	var location = req.body.location;
+	var title = req.body.title; 
+	var description = req.body.description;
+
+	//console.log(data);
+	res.render('story_1',{"location" : location, "title": title, "description": description});
 };
